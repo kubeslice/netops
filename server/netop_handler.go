d62185db2fc8db2632c465072e6c045b15a14ef1
rahulsawra98 <r.sawra@gmail.com>
2023-02-13 20:10:30 +0530

    test

--- server/netop_handler.go
+++ server/netop_handler.go
@@ -22,7 +22,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"reflect"
 
 	netops "github.com/kubeslice/netops/pkg/proto"
 
@@ -576,12 +575,34 @@ func updateSliceGwInfo(sliceID string, gwInfo *SliceGwInfo) {
 	} else {
 		// Check if sliceGW info has changed.
 		if NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].gwType != gwInfo.gwType ||
-			reflect.DeepEqual(NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].localPorts,gwInfo.localPorts) ||
-			reflect.DeepEqual(NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].remotePorts, gwInfo.remotePorts) {
+		!sameStringSlice(NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].localPorts, gwInfo.localPorts) ||
+		!sameStringSlice(NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].remotePorts, gwInfo.remotePorts) {
 			logger.GlobalLogger.Infof("slicegw info changed",gwInfo,NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId])
 			NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId] = gwInfo
 			NetOpHandle[sliceID].sliceGwInfo[gwInfo.sliceGwId].tcConfigured = false
-			
 		}
 	}
+}
+
+func sameStringSlice(x, y []string) bool {
+    if len(x) != len(y) {
+        return false
+    }
+    // create a map of string -> int
+    diff := make(map[string]int, len(x))
+    for _, _x := range x {
+        // 0 value for int is 0, so just increment a counter for the string
+        diff[_x]++
+    }
+    for _, _y := range y {
+        // If the string _y is not in diff bail out early
+        if _, ok := diff[_y]; !ok {
+            return false
+        }
+        diff[_y] -= 1
+        if diff[_y] == 0 {
+            delete(diff, _y)
+        }
+    }
+    return len(diff) == 0
 }
